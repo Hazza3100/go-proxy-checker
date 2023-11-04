557d037e2c6f7e0362ff1fb8338218188af9a78e
okpuud <149838349+okpuud@users.noreply.github.com>
2023-11-04 11:25:07 -0400

    heheheh

--- main.go
+++ main.go
@@ -10,82 +10,84 @@ import (
 	"time"
 )
 
-var wg sync.WaitGroup
+type ProxyChecker struct {
+	Proxies      []string
+	ValidProxies []string
+}
 
-func main() {
-	start := time.Now()
-	inputFile, err := os.Open("proxies.txt")
-	if err != nil {
-		fmt.Println("Failed to open input file:", err)
-		os.Exit(1)
+func NewProxyChecker(proxies []string) *ProxyChecker {
+	return &ProxyChecker{
+		Proxies:      proxies,
+		ValidProxies: make([]string, 0),
 	}
-	defer inputFile.Close()
-	outputFile, err := os.Create("valid.txt")
-	if err != nil {
-		fmt.Println("Failed to create output file:", err)
-		os.Exit(1)
+}
+
+func (pc *ProxyChecker) CheckProxy(proxy string, validChan chan<- string, httpClient *http.Client) {
+	proxyURL, _ := url.Parse("http://" + proxy)
+	httpClient.Transport = &http.Transport{Proxy: http.ProxyURL(proxyURL)}
+	response, err := httpClient.Get("https://ipinfo.io/ip")
+	if err == nil && response.StatusCode == 200 {
+		validChan <- proxy
+	} else {
+		validChan <- ""
+		fmt.Println("Invalid proxy:", proxy) // Log the invalid proxy
 	}
-	defer outputFile.Close()
-	scanner := bufio.NewScanner(inputFile)
-	writer := bufio.NewWriter(outputFile)
+}
 
-	// 12K max threads, change if u want i just dont feel the need to do more lol
-	semaphore := make(chan struct{}, 120000)
+func (pc *ProxyChecker) RunChecks() {
+	var wg sync.WaitGroup
+	validChan := make(chan string, len(pc.Proxies))
+	httpClient := &http.Client{Timeout: 5 * time.Second}
 
-	for scanner.Scan() {
-		proxy := scanner.Text()
-
+	for _, proxy := range pc.Proxies {
 		wg.Add(1)
-		go func(proxy string) {
-			semaphore <- struct{}{}
-			defer func() {
-				wg.Done()
-				<-semaphore
-			}()
-
-			if checkProxy(proxy) {
-				_, err := writer.WriteString(proxy + "\n")
-				if err != nil {
-					fmt.Println("Failed to write to file:", err)
-				} else {
-					fmt.Println("Valid proxy:", proxy)
-				}
-			} else {
-				fmt.Println("Invalid proxy:", proxy)
-			}
+		go func(p string) {
+			defer wg.Done()
+			pc.CheckProxy(p, validChan, httpClient)
 		}(proxy)
 	}
 
-	wg.Wait()
-	writer.Flush()
-	fmt.Println("Finished checking proxies!")
+	go func() {
+		wg.Wait()
+		close(validChan)
+	}()
 
-	took := time.Since(start)
-	fmt.Println("Took:", took)
+	for proxy := range validChan {
+		if proxy != "" {
+			pc.ValidProxies = append(pc.ValidProxies, proxy)
+			fmt.Println("Valid proxy found:", proxy)
+			file, err := os.OpenFile("valid.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			if _, err := file.WriteString(proxy + "\n"); err != nil {
+				fmt.Println(err)
+			}
+			file.Close()
+		}
+	}
 }
 
-func checkProxy(proxy string) bool {
-	proxyURL := &url.URL{
-		Host:   proxy,
-		Scheme: "http",
-	}
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			Proxy: http.ProxyURL(proxyURL),
-		},
-		Timeout: 3 * time.Second,
-	}
-
-	resp, err := client.Get("https://ipinfo.io/ip")
+func main() {
+	file, err := os.Open("proxy.txt")
 	if err != nil {
-		return false
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	var proxies []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		proxies = append(proxies, scanner.Text())
 	}
 
-	defer resp.Body.Close()
-	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		return true
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	return false
+	proxyChecker := NewProxyChecker(proxies)
+	proxyChecker.RunChecks()
 }
